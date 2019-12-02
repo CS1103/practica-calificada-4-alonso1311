01dd89781906f4e78ba4ba629ee5b3b0b95251c4
alonso1311 <alonso.barrios@utec.edu.pe>
2019-12-02 05:02:23 -0500

    Organizado

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-﻿#include "Filter.h"
-#include "Rotated.h"
-#include "Teacher.h"
+﻿#include "Functions/Filter.h"
+#include "Functions/Rotated.h"
+#include "Functions/Teacher.h"
 
 int main()
 {
@@ -13,20 +13,20 @@ int main()
 
     auto image_red = decode("../in.png", w, h);
     filter(image_red, w, h, 'R');
-    encode("../out_red.png", image_red, w, h);
+    encode("../Results/out_red.png", image_red, w, h);
 
     auto image_green = decode("../in.png", w, h);
     filter(image_green, w, h, 'G');
-    encode("../out_green.png", image_green, w, h);
+    encode("../Results/out_green.png", image_green, w, h);
 
     auto image_blue = decode("../in.png", w, h);
     filter(image_blue, w, h, 'B');
-    encode("../out_blue.png", image_blue, w, h);
+    encode("../Results/out_blue.png", image_blue, w, h);
 
     //--Rotate
     auto image_rotate = decode("../in.png", w, h);
     auto temp_image = rotated(image_rotate, w, h, 90);
-    encode("../out_rotated.png", temp_image, w, h);
+    encode("../Results/out_rotated.png", temp_image, w, h);
 
     return 0;
 }
